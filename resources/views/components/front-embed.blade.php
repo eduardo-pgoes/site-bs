3cf355ac2d7a0ee95a48536d7034f8f9b0b26200
Eduardo Goes <thereedwardgoes@gmail.com>
2020-06-04 09:06:54 -0300

    Criação componente - Embed do promo

--- resources/views/components/front-embed.blade.php
+++ resources/views/components/front-embed.blade.php
@@ -1,7 +1,21 @@
 <div>
     <div class="container">
-        <h2 class="text-center" style="font-weight: bold;">
-            Assista ao nosso vídeo promo!</h2>
+        <h2 class="text-center text-front-embed">
+            Assista ao nosso vídeo promo!
+        </h2>
+        <div class='row'>
+            <div class='col-sm-2'></div>
+            <div class='col-sm-8'>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe width="560" height="315"
+                        src="https://www.youtube.com/embed/9mryBUYNzDA" 
+                        frameborder="0" 
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                        allowfullscreen>
+                    </iframe>
+                </div>
+            </div>
+            <div class='col-sm-2'></div>
         </div>
     </div>
 </div>
