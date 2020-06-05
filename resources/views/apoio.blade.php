1eeadf2f1ecef879c4db642feda7c1024491bc67
Silva <silva@localhost.localdomain>
2020-06-05 00:36:31 -0300

    criação pagina de apoio

--- resources/views/apoio.blade.php
+++ resources/views/apoio.blade.php
@@ -1,9 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
-
-    Não implementado
-
+    <div class="container">
     
-    <!--TODO Preencher com as fotos-->
+        Apoio
+
+
+        <div>
+            <p>
+            (insira texto fofinho aqui)
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Modi soluta, commodi quae quis tempora excepturi.
+            Doloremque eius et, quidem quasi suscipit aliquid beatae vero vitae blanditiis. 
+            Ipsum aspernatur quas reprehenderit?
+            </p>    
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <img src = "assets/apoio/3D Tecnologia Preto.png" style="width: 500px; height:300px;" >
+                </div>
+                <div class="col">
+                    <img src = "assets/apoio/Collins Aerospace.png" style="width: 500px; height:300px;" >
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <img src = "assets/apoio/FESMO.png" style="width: 500px; height:300px;" >
+                </div>
+                <div class="col">
+                    <img src = "assets/apoio/IFSP.png" style="width: 500px; height:300px;" >
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <img src = "assets/apoio/micro-assit.png" style="width: 500px; height:300px;" >
+                </div>
+                <div class="col">
+                    <img src = "assets/apoio/STC.png" style="width: 500px; height:300px;" >
+                </div>
+            </div>
+            <div class="row justify-content-md-center">
+                <div class="col">
+                    <img src = "assets/apoio/Usimaza-azul.png" style="width: 500px; height:300px;" >
+                </div>
+            </div>
+        </div>
+        <div>
+            <p>
+            (insira texto fofinho aqui, só que agora sobre a ajapet)
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Modi soluta, commodi quae quis tempora excepturi.
+            Doloremque eius et, quidem quasi suscipit aliquid beatae vero vitae blanditiis. 
+            Ipsum aspernatur quas reprehenderit?
+            </p> 
+
+            <img src = "assets/apoio/AJAPET.png" style="width:900px; height:400px;" >
+        </div>
+    </div>    
+    <!--TODO Preencher com as fotos
+    
+    
+    modus:
+    
+    add text
+    add grid
+    
+    add text 
+    add foto ajapet
+    -->
 @endsection
